d31660eacab52a59fef81e4102e5bfacfdb74331
dartagnan009 <62113603+dartagnan009@users.noreply.github.com>
2021-11-29 13:03:42 -0600

    Add files via upload

--- ManagmentWindow.java
+++ ManagmentWindow.java
@@ -1,351 +1,189 @@
 package Main;
-
 import javax.swing.*;
-import java.util.ArrayList;
-import java.util.Scanner;
-import java.util.Vector;
-import java.util.List;
-import java.lang.*;
-import java.sql.*;
 import javax.swing.border.EmptyBorder;
-import javax.swing.event.DocumentEvent;
-import javax.swing.event.DocumentListener;
 import javax.swing.table.DefaultTableCellRenderer;
-import javax.swing.table.DefaultTableModel;
-
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
-
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class ManagmentWindow extends JFrame implements ActionListener {
-	
-
-
-    ManagmentWindow(){
-        creatNewAccountGUI();
 		
+	JFrame frame = new JFrame("Management");
+	JTextField firstName = new JTextField(15);
+    JTextField lastName = new JTextField(15);
+    JTextField ssn = new JTextField(15);
+    JTextField address = new JTextField(15);
+    JTextField city = new JTextField(15);
+    JTextField state = new JTextField(15);
+    JTextField phone = new JTextField(15);
+    JLabel lbFirstNameLabel= new JLabel("First Name");
+    JLabel lbLastNameLabel = new JLabel("Last Name");    
+    JLabel lbSSNLabel = new JLabel("SSN");
+    JLabel lbAddressLabel = new JLabel("Street Address");
+    JLabel lbCityLabel = new JLabel("City");
+    JLabel lbStateLabel = new JLabel("State");
+    JLabel lbPhoneLabel = new JLabel("Phone number");
+    JButton submit = new JButton("Insert Account");
+    String ssn_string;
+    String fname_string;
+    String lname_string;
+    String address_string;
+    String city_string;
+    String state_string;
+    String phone_string;
+    GridBagLayout bagLayout = new GridBagLayout();
+	GridBagConstraints bagConstraints = new GridBagConstraints();
 
-    }
+	ManagmentWindow(){
+
+    	frame.setLayout(bagLayout);
+		bagConstraints.insets = new Insets(15, 40, 0, 0);
+		
+		bagConstraints.gridx = 0;
+		bagConstraints.gridy = 0;
+		frame.add(lbFirstNameLabel, bagConstraints);
+		
+		bagConstraints.gridx = 1;
+		bagConstraints.gridy = 0;
+		frame.add(firstName, bagConstraints);
+		
+		bagConstraints.gridx = 0;
+		bagConstraints.gridy = 1;
+		frame.add(lbLastNameLabel, bagConstraints);
+		
+		bagConstraints.gridx = 1;
+		bagConstraints.gridy = 1;
+		frame.add(lastName, bagConstraints);
+		
+		bagConstraints.gridx = 0;
+		bagConstraints.gridy = 2;
+		frame.add(lbSSNLabel, bagConstraints);
+		
+		bagConstraints.gridx = 1;
+		bagConstraints.gridy = 2;
+		frame.add(ssn, bagConstraints);
+		
+		bagConstraints.gridx = 0;
+		bagConstraints.gridy = 3;
+		frame.add(lbAddressLabel, bagConstraints);
+		
+		bagConstraints.gridx = 1;
+		bagConstraints.gridy = 3;
+		frame.add(address, bagConstraints);
+		
+		bagConstraints.gridx = 0;
+		bagConstraints.gridy = 4;
+		frame.add(lbCityLabel, bagConstraints);
+		
+		bagConstraints.gridx = 1;
+		bagConstraints.gridy = 4;
+		frame.add(city, bagConstraints);
+		
+		bagConstraints.gridx = 0;
+		bagConstraints.gridy = 5;
+		frame.add(lbStateLabel, bagConstraints);
+		
+		bagConstraints.gridx = 1;
+		bagConstraints.gridy = 5;
+		frame.add(state, bagConstraints);
+		
+		bagConstraints.gridx = 0;
+		bagConstraints.gridy = 6;
+		frame.add(lbPhoneLabel, bagConstraints);
+		
+		bagConstraints.gridx = 1;
+		bagConstraints.gridy = 6;
+		frame.add(phone, bagConstraints);
+		
+		bagConstraints.gridx = 2;
+		bagConstraints.gridy = 3;
+		frame.add(submit, bagConstraints);
+		
+		submit.addActionListener(this);
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(true);
+        frame.setSize(500,300);
+        frame.setVisible(true);
+
+	}
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        // TODO Auto-generated method stub
+    	if(e.getSource() == submit) {
+    			
+    		fname_string = firstName.getText();
+    		lname_string = lastName.getText();
+    		ssn_string = ssn.getText();
+    		address_string = address.getText();
+    		city_string = city.getText();
+    		state_string = state.getText();
+    		phone_string = phone.getText();
+    
+    		//invalid criteria
+    		if((fname_string == null) || (lname_string == null) || (ssn_string == null) || (address_string == null) || (city_string == null) || (state_string == null) || (phone_string == null)) {
+				JOptionPane.showConfirmDialog(null, "Invalid Criteria", "Result",JOptionPane.DEFAULT_OPTION);
+				System.exit(0);
+			}
+    		//ssn is already existing
+	        else if (checkSSN(ssn_string) == true) {
+	        	JOptionPane.showConfirmDialog(null,"Borrower Already exists", "Result", JOptionPane.DEFAULT_OPTION);
+				System.exit(0);
+	        }
+    		//new account can be made
+			else{
 
+		        try {
+		      
+		            Class.forName("com.mysql.cj.jdbc.Driver");
+		            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/library?autoReconnect=true", "root", "Thecrow_69");               		
+		            Statement s = conn.createStatement();
+		            
+		            String sql = "SELECT COUNT(*) FROM borrower;";
+		            ResultSet rs = s.executeQuery(sql);
+		            rs.next();
+		            String max = rs.getString(1); //gets 1000 or the highest count
+
+	        		int newCardID = Integer.parseInt(max) + 1;
+	        		
+	        			address_string += ", " + city_string + ", " + state_string;
+	                    String bname = fname_string +" "+ lname_string; 
+	                    String insert_stmt = "Insert into BORROWER (card_id, ssn, bname, address, phone) Values ('" + newCardID + "', '" + ssn_string + "', '" + bname + "', '"  + address_string + "', '" + phone_string + "')";
+	                    int insert = s.executeUpdate(insert_stmt);
+	                    JOptionPane.showConfirmDialog(null, "New account has been created", "Result" , JOptionPane.DEFAULT_OPTION);
+	                    System.exit(0);
+	                    
+		        }catch(Exception e1) {
+		            JOptionPane.showMessageDialog(null, e1);
+		        }		
+			}			
+    	}
     }
+    
+    public Boolean checkSSN(String ssn){
 
-	
+        try {
 
-	public void creatNewAccountGUI(){
-		JFrame frame = new JFrame("Mangament");
-		JPanel pnPanel0;
-		JLabel lbNewAccountTitle;
-		JLabel lbNameLable;
-		JLabel lbSSNLable;
-		JLabel lbAddressLable;
-		JLabel lbLastNameLable;
-		JLabel lbPhoneNumberLabel;
-		JButton btCreateAccountButton;
-		JTextField tfNameText;
-		JTextField tfSSNText;
-		JTextField tfAddressText;
-		JTextField tfLastNameText;
-		JTextField tfPhoneNumberText;
-		
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/library?autoReconnect=true", "root", "Thecrow_69");               		
+            Statement s = conn.createStatement();
+            String sql = "SELECT EXISTS(SELECT Ssn FROM borrower WHERE borrower.ssn = '" + ssn + "')";
+            ResultSet rs = s.executeQuery(sql);
+            rs.next();
+            boolean test = rs.getBoolean(1);
+            
+    		if(test == true) {	
+				return true;
+    		}
 
-
-		pnPanel0 = new JPanel();
-		GridBagLayout gbPanel0 = new GridBagLayout();
-		GridBagConstraints gbcPanel0 = new GridBagConstraints();
-		pnPanel0.setLayout( gbPanel0 );
-
-		lbNewAccountTitle = new JLabel( "          New Account"  );
-		gbcPanel0.gridx = 5;
-		gbcPanel0.gridy = 1;
-		gbcPanel0.gridwidth = 9;
-		gbcPanel0.gridheight = 2;
-		gbcPanel0.fill = GridBagConstraints.BOTH;
-		gbcPanel0.weightx = 1;
-		gbcPanel0.weighty = 1;
-		gbcPanel0.anchor = GridBagConstraints.CENTER;
-		gbcPanel0.insets = new Insets( 0,17,0,0 );
-		gbPanel0.setConstraints( lbNewAccountTitle, gbcPanel0 );
-		pnPanel0.add( lbNewAccountTitle );
-
-		lbNameLable = new JLabel( "First Name"  );
-		gbcPanel0.gridx = 2;
-		gbcPanel0.gridy = 4;
-		gbcPanel0.gridwidth = 7;
-		gbcPanel0.gridheight = 2;
-		gbcPanel0.fill = GridBagConstraints.BOTH;
-		gbcPanel0.weightx = 1;
-		gbcPanel0.weighty = 1;
-		gbcPanel0.anchor = GridBagConstraints.NORTH;
-		gbPanel0.setConstraints( lbNameLable, gbcPanel0 );
-		pnPanel0.add( lbNameLable );
-
-		lbLastNameLable = new JLabel( "Last Name"  );
-		gbcPanel0.gridx = 2;
-		gbcPanel0.gridy = 7;
-		gbcPanel0.gridwidth = 7;
-		gbcPanel0.gridheight = 2;
-		gbcPanel0.fill = GridBagConstraints.BOTH;
-		gbcPanel0.weightx = 1;
-		gbcPanel0.weighty = 1;
-		gbcPanel0.anchor = GridBagConstraints.NORTH;
-		gbPanel0.setConstraints( lbLastNameLable, gbcPanel0 );
-		pnPanel0.add( lbLastNameLable );
-
-		lbSSNLable = new JLabel( "SSN"  );
-		gbcPanel0.gridx = 2;
-		gbcPanel0.gridy = 10;
-		gbcPanel0.gridwidth = 7;
-		gbcPanel0.gridheight = 2;
-		gbcPanel0.fill = GridBagConstraints.BOTH;
-		gbcPanel0.weightx = 1;
-		gbcPanel0.weighty = 1;
-		gbcPanel0.anchor = GridBagConstraints.NORTH;
-		gbPanel0.setConstraints( lbSSNLable, gbcPanel0 );
-		pnPanel0.add( lbSSNLable );
-
-		lbAddressLable = new JLabel( "Address"  );
-		gbcPanel0.gridx = 2;
-		gbcPanel0.gridy = 13;
-		gbcPanel0.gridwidth = 7;
-		gbcPanel0.gridheight = 2;
-		gbcPanel0.fill = GridBagConstraints.BOTH;
-		gbcPanel0.weightx = 1;
-		gbcPanel0.weighty = 1;
-		gbcPanel0.anchor = GridBagConstraints.NORTH;
-		gbPanel0.setConstraints( lbAddressLable, gbcPanel0 );
-		pnPanel0.add( lbAddressLable );
-
-		lbPhoneNumberLabel = new JLabel( "Phone Number"  );
-		gbcPanel0.gridx = 2;
-		gbcPanel0.gridy = 16;
-		gbcPanel0.gridwidth = 7;
-		gbcPanel0.gridheight = 2;
-		gbcPanel0.fill = GridBagConstraints.BOTH;
-		gbcPanel0.weightx = 1;
-		gbcPanel0.weighty = 1;
-		gbcPanel0.anchor = GridBagConstraints.NORTH;
-		gbPanel0.setConstraints( lbPhoneNumberLabel, gbcPanel0 );
-		pnPanel0.add( lbPhoneNumberLabel );
-
-		
-
-		btCreateAccountButton = new JButton( "Create Account"  );
-		gbcPanel0.gridx = 5;
-		gbcPanel0.gridy = 20;
-		gbcPanel0.gridwidth = 9;
-		gbcPanel0.gridheight = 2;
-		gbcPanel0.fill = GridBagConstraints.BOTH;
-		gbcPanel0.weightx = 1;
-		gbcPanel0.weighty = 0;
-		gbcPanel0.anchor = GridBagConstraints.NORTH;
-		gbcPanel0.insets = new Insets( 0,29,0,0 );
-		gbPanel0.setConstraints( btCreateAccountButton, gbcPanel0 );
-		pnPanel0.add( btCreateAccountButton );
-
-
-
-		tfNameText = new JTextField( );
-		gbcPanel0.gridx = 12;
-		gbcPanel0.gridy = 4;
-		gbcPanel0.gridwidth = 8;
-		gbcPanel0.gridheight = 2;
-		gbcPanel0.fill = GridBagConstraints.BOTH;
-		gbcPanel0.weightx = 1;
-		gbcPanel0.weighty = 0;
-		gbcPanel0.anchor = GridBagConstraints.NORTH;
-		gbPanel0.setConstraints( tfNameText, gbcPanel0 );
-		pnPanel0.add( tfNameText );
-
-		tfLastNameText = new JTextField( );
-		gbcPanel0.gridx = 12;
-		gbcPanel0.gridy = 7;
-		gbcPanel0.gridwidth = 8;
-		gbcPanel0.gridheight = 2;
-		gbcPanel0.fill = GridBagConstraints.BOTH;
-		gbcPanel0.weightx = 1;
-		gbcPanel0.weighty = 0;
-		gbcPanel0.anchor = GridBagConstraints.NORTH;
-		gbPanel0.setConstraints( tfLastNameText, gbcPanel0 );
-		pnPanel0.add( tfLastNameText );
-
-		
-
-		tfSSNText = new JTextField( );
-		gbcPanel0.gridx = 12;
-		gbcPanel0.gridy = 10;
-		gbcPanel0.gridwidth = 8;
-		gbcPanel0.gridheight = 2;
-		gbcPanel0.fill = GridBagConstraints.BOTH;
-		gbcPanel0.weightx = 1;
-		gbcPanel0.weighty = 0;
-		gbcPanel0.anchor = GridBagConstraints.NORTH;
-		gbPanel0.setConstraints( tfSSNText, gbcPanel0 );
-		pnPanel0.add( tfSSNText );
-
-		tfAddressText = new JTextField( );
-		gbcPanel0.gridx = 12;
-		gbcPanel0.gridy = 13;
-		gbcPanel0.gridwidth = 8;
-		gbcPanel0.gridheight = 2;
-		gbcPanel0.fill = GridBagConstraints.BOTH;
-		gbcPanel0.weightx = 1;
-		gbcPanel0.weighty = 0;
-		gbcPanel0.anchor = GridBagConstraints.NORTH;
-		gbPanel0.setConstraints( tfAddressText, gbcPanel0 );
-		pnPanel0.add( tfAddressText );
-        
-		tfPhoneNumberText = new JTextField( );
-		gbcPanel0.gridx = 12;
-		gbcPanel0.gridy = 16;
-		gbcPanel0.gridwidth = 8;
-		gbcPanel0.gridheight = 2;
-		gbcPanel0.fill = GridBagConstraints.BOTH;
-		gbcPanel0.weightx = 1;
-		gbcPanel0.weighty = 0;
-		gbcPanel0.anchor = GridBagConstraints.NORTH;
-		gbPanel0.setConstraints( tfPhoneNumberText, gbcPanel0 );
-		pnPanel0.add( tfPhoneNumberText );
-
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.add(pnPanel0);
-		frame.setResizable(true);
-        frame.setSize(1000,1000);
-        frame.setVisible(true);
-
-
-		tfNameText.getDocument().addDocumentListener((DocumentListener) new DocumentListener() {
-			public void changedUpdate(DocumentEvent e) {
-			  changed();
-			}
-			public void removeUpdate(DocumentEvent e) {
-			  changed();
-			}
-			public void insertUpdate(DocumentEvent e) {
-			  changed();
-			}
-		  
-			public void changed() {
-				btCreateAccountButton.setEnabled(validatess(tfSSNText, tfNameText, tfLastNameText, tfAddressText, tfPhoneNumberText));
-			}
-		  });
-
-tfSSNText.getDocument().addDocumentListener((DocumentListener) new DocumentListener() {
-			public void changedUpdate(DocumentEvent e) {
-			  changed();
-			}
-			public void removeUpdate(DocumentEvent e) {
-			  changed();
-			}
-			public void insertUpdate(DocumentEvent e) {
-			  changed();
-			}
-		  
-			public void changed() {
-				btCreateAccountButton.setEnabled(validatess(tfSSNText, tfNameText, tfLastNameText, tfAddressText, tfPhoneNumberText));
-
-		  
-			}
-		  });
-
-		  tfAddressText.getDocument().addDocumentListener((DocumentListener) new DocumentListener() {
-			public void changedUpdate(DocumentEvent e) {
-			  changed();
-			}
-			public void removeUpdate(DocumentEvent e) {
-			  changed();
-			}
-			public void insertUpdate(DocumentEvent e) {
-			  changed();
-			}
-		  
-			public void changed() {
-				btCreateAccountButton.setEnabled(validatess(tfSSNText, tfNameText, tfLastNameText, tfAddressText, tfPhoneNumberText));
-
-		  
-			}
-		  });
-		  
-		  tfLastNameText.getDocument().addDocumentListener((DocumentListener) new DocumentListener() {
-			public void changedUpdate(DocumentEvent e) {
-			  changed();
-			}
-			public void removeUpdate(DocumentEvent e) {
-			  changed();
-			}
-			public void insertUpdate(DocumentEvent e) {
-			  changed();
-			}
-		  
-			public void changed() {
-				btCreateAccountButton.setEnabled(validatess(tfSSNText, tfNameText, tfLastNameText, tfAddressText, tfPhoneNumberText));
-
-		  
-			}
-		  });
-		  
-		  tfPhoneNumberText.getDocument().addDocumentListener((DocumentListener) new DocumentListener(){
-			public void changedUpdate(DocumentEvent e) {
-			  changed();
-			}
-			public void removeUpdate(DocumentEvent e) {
-			  changed();
-			}
-			public void insertUpdate(DocumentEvent e) {
-			  changed();
-			}
-		  
-			public void changed() {
-				btCreateAccountButton.setEnabled(validatess(tfSSNText, tfNameText, tfLastNameText, tfAddressText, tfPhoneNumberText));
-
-				
-			}
-		  });
-
-
-
-
-
-
-
-
-		btCreateAccountButton.addActionListener(new ActionListener(){
-			
-			@Override
-            public void actionPerformed(ActionEvent e) {
-				
-				
-					person newPerson = new person(tfNameText.getText(),tfLastNameText.getText() , tfSSNText.getText(),tfAddressText.getText(),tfPhoneNumberText.getText());
-
-
-					int veriNum = newPerson.verifyiPerson();
-					if(veriNum == -1){
-						JOptionPane.showMessageDialog(frame, "SSN is alredy being used");
-					}
-					else if(veriNum == 0){
-						newPerson.addPerson();
-						JOptionPane.showMessageDialog(frame, "New account has been created");
-						frame.setVisible(false); //you can't see me!
-						frame.dispose(); //Destroy the JFrame object					
-					}
-					
-				}
-				
-			
-
-		});
-	}
-
-	public boolean validatess(JTextField tfSSNText, JTextField tfNameText, JTextField tfLastNameText, JTextField tfAddressText, JTextField tfPhoneNumberText){
-
-
-		return !tfSSNText.getText().equals("") && !tfNameText.getText().equals("") && !tfLastNameText.getText().equals("")&&!tfAddressText.getText().equals("")&&!tfPhoneNumberText.getText().equals("");
-	}
-
+        }catch(Exception e1) {
+            JOptionPane.showMessageDialog(null, e1);
+        }
+        return false;	
+    }
 }
-
